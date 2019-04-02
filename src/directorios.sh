782d0c076e5e69366bd195226be7be2ebbbe468e
MiguiTE <miguite19@gmail.com>
2019-04-02 12:39:46 +0200

    Crear directorios
    Código para crear el esquema de directorios para guardar datos,
    modelos y predicciones.

---
+++ src/directorios.sh
@@ -0,0 +1,20 @@
+mkdir data
+cd data
+mkdir datosEstaciones modelos resultados
+for tipo in $(ls)
+do
+    mkdir ${tipo}/primavera ${tipo}/verano ${tipo}/otoño ${tipo}/invierno
+
+    for dir in $(ls ${tipo})
+    do
+        mkdir ${tipo}/${dir}/lluvia ${tipo}/${dir}/tmax ${tipo}/${dir}/tmin
+        for subdir in $(ls ${tipo}/${dir})
+        do
+            mkdir ${tipo}/${dir}/${subdir}/GLM-KNN
+            mkdir ${tipo}/${dir}/${subdir}/RF
+            mkdir ${tipo}/${dir}/${subdir}/NNRF
+        done
+    done
+done
+
+
