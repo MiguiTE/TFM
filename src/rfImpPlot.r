ed257dd621c8e86c87bf01c6a63d165555c32920
MiguiTE <miguite19@gmail.com>
2019-05-24 18:03:07 +0200

    Finaliza dibujos de importancia
    Lollipops para la importancia de las variables
    según criterio de medida y ECM usado

--- src/rfImpPlot.r
+++ src/rfImpPlot.r
@@ -1,44 +1,43 @@
-ruta = "/home/jovyan/TFM/TFM/"
+library(tidyverse)
+
+ruta = "/home/doctor/workspace/master/TFM/"
 
 gcms = c("Canes", "Cnrm", "Gfdl", "Miroc", "MpiLr", "MpiMr", "Noresm")
-
-hist = list()
-hist[["Canes"]] = "canesm2.historical"
-hist[["Cnrm"]] = "cnrm.historial"
-hist[["Gfdl"]] = "gfdl.historical"
-hist[["Miroc"]] = "miroc.historical"
-hist[["MpiLr"]] = "mpi.lr.historical"
-hist[["MpiMr"]] = "mpi.mr.historical"
-hist[["Noresm"]] = "noresm.historical"
-
-rcp85 = list()
-rcp85[["Canes"]] = "canesm2.rcp85"
-rcp85[["Cnrm"]] = "cnrm.rcp85"
-rcp85[["Gfdl"]] = "gfdl.rcp85"
-rcp85[["Miroc"]] = "miroc.rcp85"
-rcp85[["MpiLr"]] = "mpi.lr.rcp85"
-rcp85[["MpiMr"]] = "mpi.mr.rcp85"
-rcp85[["Noresm"]] = "noresm.rcp85"
-
-patrones = list()
-patrones[["P1"]] = c("psl", "ta@85000")
-patrones[["P2"]] = c("psl", "hus@85000")
-patrones[["P3"]] = c("psl", "ta@85000", "hus@85000")
-patrones[["P4"]] = c("psl", "ta@85000", "ua@25000","hus@85000")
-patrones[["P5"]] = c("psl", "hur@85000")
-patrones[["P6"]] = c("psl", "hur@85000", "ua@25000")
-patrones[["P7"]] = c("psl", "ta@85000", "ua@25000","hus@85000", "hur@85000")
-patrones[["P8"]] = c("hur@25000", "hur@50000", "hur@85000", "hus@25000", "hus@50000", "hus@85000", "ta@25000", "ta@50000", "ta@85000", "ua@25000", "ua@50000", "ua@85000", "va@25000", "va@50000", "va@85000", "psl")
-opcionPatrones = c("P7")
-
+variables = c("psl", "T850", "U250","Q850", "R850")
+patron = "P7"
 nvecinos = c(1, 25)
 
-colores = 
-
+df = list()
 for (n in nvecinos){
-  for (patron in opcionPatrones){
+  df[[as.character(n)]] = data.frame()
     for(gcm in gcms){
       load(paste0(ruta, "data/proyeccion/modelos/importancia", gcm, "patron", patron, "nvecions",n, "RF.rda"))
+      tmp = data.frame(cbind(x = variables, cat = rep(gcm, length(variables))), impFinalOcc, impFinalReg)
+      df[[as.character(n)]] = rbind(df[[as.character(n)]], tmp)
+    }
+}
+
+
+importancias = c("MeanDecreaseGini", "MeanDecreaseAccuracy", "X.IncMSE", "IncNodePurity")
+GUARDA = F
+for (n in nvecinos) {
+  for (importancia in importancias) {
+    if (GUARDA){
+      pdf(paste0(ruta, "imagenes/importancia",importancia, "vecinos", n, ".pdf", collapse = "")) 
+    }
+    print(ggplot(df[[as.character(n)]], aes(x = x, y = get(importancia), color = cat)) + 
+      geom_segment(aes(x=x, xend=x, y=0, yend=get(importancia)), color = "skyblue") +
+      geom_point(size=4, alpha=0.6) +
+      theme_light() +
+      coord_flip() +
+      theme(
+        panel.grid.major.y = element_blank(),
+        panel.border = element_blank(),
+        axis.ticks.y = element_blank()
+      ) +
+      labs(x = "", y ="Importancia", title = paste(importancia, "vecinos", n), color = "ECM"))
+    if (GUARDA){
+      dev.off()
     }
   }
 }
