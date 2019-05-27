028122e76c1c78bdbc10c4ccf328432306ae0fa5
MiguiTE <miguite19@gmail.com>
2019-05-27 13:57:47 +0200

    Mejora figura de value

--- src/rf.r
+++ src/rf.r
@@ -26,11 +26,23 @@ spatialPlot(clim[[vars[7]]], backdrop.theme = "countries", main = vars[7])
 
 #Observaciones
 library(RColorBrewer)
-library(sp)
 obs = loadStationData(paste0(ruta, "data/VALUE_ECA_86_v2.zip",collapse = ""), var = "precip")
-sp = SpatialPoints(getCoordinates(obs))
 url = "http://meteo.unican.es/tds5/dodsC/eobs/e-obs_v17_0.25regular.ncml"
-colores = c("#a4eff7", "#85c99c", "#28871f", "#616d0d", "#935f00", "#b77600", "#c88200", "#d79d33", "#f1debb")
+coloresO = c("#a4eff7", "#85c99c", "#28871f", "#616d0d", "#935f00", "#b77600", "#c88200", "#d79d33", "#f1debb")
 orografia = loadGridData(dataset = url, var = "elevation", lonLim = c(-10, 32), latLim = c(36,72))
 orografia$Data[is.na(orografia$Data)] = 0
-spatialPlot(orografia, backdrop.theme = "countries", col.regions  = colorRampPalette(colores)) + spatialPlot(climatology(obs))
+spatialPlot(orografia, backdrop.theme = "countries", col.regions  = colorRampPalette(coloresO)) + spatialPlot(climatology(obs))
+
+
+#Con regiones
+longitudes_y <- matrix(c(-10,2,-10,3,-5,5,3,16,3,32,5,16,3,25,16,30), nrow = 2, ncol = 8)
+latitudes_y <- matrix(c(50,59,36,44,44,50,48,55,55,72,44,48,36,44,44,55), nrow = 2, ncol = 8)
+
+coloresR = c("blue", "orange", "green", "cyan", "deepskyblue", "darkgreen", "red", "violet")
+regiones = list()
+for(i in 1:8){
+  regiones[[i]] = map.lines(lonLim = (longitudes_y[,i] - c(-0.09, 0.09)), latLim = (latitudes_y[,i] - c(-0.09, 0.09)), col = coloresR[i], lwd = 3)  
+}
+
+spatialPlot(orografia, backdrop.theme = "countries", col.regions  = colorRampPalette(coloresO)) + spatialPlot(climatology(obs), sp.layout = regiones)
+
